bf8797109f4725be015c19c0fafcc6254df278e3
Ahmad Qanadilo <AhmadQan@estartasolutions.com>
2023-01-12 13:31:18 +0300

    adding theme

--- pages/places/index.js
+++ pages/places/index.js
@@ -1,14 +1,12 @@
 import React, { useEffect } from "react";
-import axios from "axios";
 import { fetchPlaces } from "../../storeSlices/placesSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function PlacesGrid() {
-  const dispatch = useDispatch();
   const PlacesStore = useSelector((state) => state.places);
   const { allPlaces, loading, err, totalCount } = PlacesStore;
 
-  console.log(totalCount);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchPlaces(5, 1));
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: "#122b50",
+      secondry: "#1a9edc",
+      accent: "#28d580",
+      grayDark: "#273444",
+      gray: "#8492a6",
+      grayLight: "#d3dce6",
+    },
+    fontFamily: {
+      Koulen: ["Koulen"],
+      IBMPlexSans: ["IBM Plex Sans", "sans-serif"],
+      IBMPlexSansArabic: ["IBM Plex Sans Arabic", "sans-serif"],
+    },
+
     extend: {},
   },
   plugins: [],
