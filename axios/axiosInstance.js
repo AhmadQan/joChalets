09e50e88e70dc6555f944bf708ade172c65dd928
Ahmad Qanadilo <AhmadQan@estartasolutions.com>
2023-01-19 00:44:45 +0300

    replacing the base API

--- axios/axiosInstance.js
+++ axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const https = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://jo-chalets.vercel.app/api/",
   headers: {
     "Content-Type": "application/json",
   },
