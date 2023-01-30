435c60c722ee87e1d14a3202e2829172d36e08d2
Ahmad Qanadilo <AhmadQan@estartasolutions.com>
2023-01-30 23:11:12 +0300

    fixing step size bug

--- components/molecules/Step.js
+++ components/molecules/Step.js
@@ -5,7 +5,7 @@ import { stylesObj } from "../../styles/stylesSpecific";
 function Step({ number = 1, children }) {
   return (
     <div
-      className={`w-20 text-xs aspect-video rounded-md shadow-flat text-center flex justify-center items-center gap-0 relative`}
+      className={`w-[20%] text-xs aspect-video rounded-md shadow-flat text-center flex justify-center items-center gap-0 relative`}
     >
       {children}
       <div className="absolute w-6 aspect-square rounded-full bg-secondry text-primaryLigther font-bold text-base -top-2 -right-2">
