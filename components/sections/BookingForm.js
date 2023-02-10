55b276da46d096bd4c80319229da6e75a61bd74e
Ahmad Qanadilo <AhmadQan@estartasolutions.com>
2023-02-10 23:01:24 +0300

    styling the order form v2

--- components/sections/BookingForm.js
+++ components/sections/BookingForm.js
@@ -11,25 +11,34 @@ function BookingForm() {
 
   const Step1 = () => {
     return (
-      <form className="h-[82.1%] flex flex-col justify-between ">
-        <div className="flex w-full justify-start gap-3 items-center p-2 rounded-xl h-auto ">
-          <h2 className="text-base font-bold text-secondryDark">From</h2>
-
-          <h2 className="text-base font-bold text-secondryDarker">12/3/2023</h2>
+      <form className="h-[78%] flex flex-col gap-3 justify-between ">
+        <div className="flex h-[9%] ">
+          <div className="flex  w-full justify-start gap-3 items-center  rounded-xl ">
+            <div className="h-full greenGradient1 w-[40%] rounded-full   flex justify-center items-center">
+              <h2 className="text-base font-bold text-white">From</h2>
+            </div>
+            <h2 className="text-sm font-bold text-secondryDarker">12/3/2023</h2>
+          </div>
+          <div className="flex  w-full justify-center gap-3 items-center  rounded-xl ">
+            <div className="h-full greenGradient1 w-[40%] rounded-full  opacity-30 flex justify-center items-center">
+              <h2 className="text-base font-bold text-white">To</h2>
+            </div>
+          </div>
         </div>
-        <div className="flex justify-between items-center w-full p-2">
-          <div className="w-[40%] flex gap-2 ">
+
+        <div className="flex  justify-start items-start h-auto w-full gap-4 pt-2">
+          <div className=" flex gap-2 shadow-flat px-1 py-2 rounded-full">
             <SunOutlineIcon fill={"#333"} className={"h-6 aspect-square"} />
             <p>10 AM</p>
             <input type={"checkbox"} />
           </div>
-          <div className="w-[45%] flex gap-2 ">
+          <div className=" flex gap-2 px-1 py-2 shadow-flat rounded-full">
             <MoonOutlineIcon fill={"#333"} className={"h-6 aspect-square"} />
             <p>10 PM</p>
             <input type={"checkbox"} />
           </div>
         </div>
-        <div className="relative w-full overflow-hidden  ">
+        <div className="relative w-full overflow-hidden  rounded-xl">
           <Calendar onChange={HandleChange} />
         </div>
         <div className="h-[10%] w-full  flex justify-between p-2">
@@ -42,7 +51,7 @@ function BookingForm() {
               setStep(2);
             }}
             // disabled={true}
-            className="w-[45%] aspect-btnOutlined bg-secondryBase text-secondryDarker font-bold rounded-xl  disabled:bg-gray-400 disabled:text-gray-200"
+            className="w-[45%] aspect-btnOutlined greenGradient1 text-secondryLigther  font-bold rounded-xl  disabled:bg-gray-400 disabled:text-gray-200"
           >
             Next
           </button>
@@ -52,26 +61,36 @@ function BookingForm() {
   };
   const Step2 = () => {
     return (
-      <form className="h-[82.1%] flex flex-col justify-between ">
-        <div className="flex w-full justify-end gap-3 items-center p-2 rounded-xl h-auto ">
-          <h2 className="text-base font-bold text-secondryDark">To</h2>
-
-          <h2 className="text-base font-bold text-secondryDarker">12/3/2023</h2>
+      <form className="h-[78%] flex flex-col gap-3 justify-between ">
+        <div className="flex h-[9%] ">
+          <div className="flex opacity-30 w-full justify-start gap-3 items-center  rounded-xl ">
+            <div className="h-full greenGradient1 w-[40%] rounded-full   flex justify-center items-center">
+              <h2 className="text-base font-bold  text-white">From</h2>
+            </div>
+            <h2 className="text-sm font-bold text-secondryDarker">12/3/2023</h2>
+          </div>
+          <div className="flex  w-full justify-center gap-3 items-center  rounded-xl ">
+            <div className="h-full greenGradient1 w-[40%] rounded-full   flex justify-center items-center">
+              <h2 className="text-base font-bold text-white">To</h2>
+            </div>
+            <h2 className="text-sm font-bold text-secondryDarker">12/3/2023</h2>
+          </div>
         </div>
-        <div className="flex justify-between items-center w-full p-2">
-          <div className="w-[40%] flex gap-2 ">
+
+        <div className="flex  justify-end items-start h-auto w-full gap-4 pt-2">
+          <div className=" flex gap-2 shadow-flat px-1 py-2 rounded-full">
             <SunOutlineIcon fill={"#333"} className={"h-6 aspect-square"} />
             <p>10 AM</p>
             <input type={"checkbox"} />
           </div>
-          <div className="w-[45%] flex gap-2 ">
+          <div className=" flex gap-2 px-1 py-2 shadow-flat rounded-full">
             <MoonOutlineIcon fill={"#333"} className={"h-6 aspect-square"} />
             <p>10 PM</p>
             <input type={"checkbox"} />
           </div>
         </div>
-        <div className="relative w-full overflow-hidden  ">
-          <Calendar date={new Date()} onChange={HandleChange} />
+        <div className="relative w-full overflow-hidden  rounded-xl">
+          <Calendar onChange={HandleChange} />
         </div>
         <div className="h-[10%] w-full  flex justify-between p-2">
           <button className="w-[45%] aspect-btnOutlined border border-secondryDark rounded-xl">
@@ -83,7 +102,7 @@ function BookingForm() {
               setStep(3);
             }}
             // disabled={true}
-            className="w-[45%] aspect-btnOutlined bg-secondryBase text-secondryDarker font-bold rounded-xl  disabled:bg-gray-400 disabled:text-gray-200"
+            className="w-[45%] aspect-btnOutlined greenGradient1 text-secondryLigther  font-bold rounded-xl  disabled:bg-gray-400 disabled:text-gray-200"
           >
             Next
           </button>
@@ -94,7 +113,7 @@ function BookingForm() {
 
   const Step3 = () => {
     return (
-      <form className="h-[82.1%] w-full  flex flex-col justify-start gap-10  ">
+      <form className="h-[78%] w-full  flex flex-col justify-start gap-10  ">
         <div className="w-full flex flex-col gap-3">
           <div className="flex justify-between items-center w-full">
             <h3 className="text-sm text-primaryDark">
@@ -149,7 +168,7 @@ function BookingForm() {
     console.log(date);
   };
   return (
-    <div className="h-[92vh] w-full  flex justify-center items-center font-IBMPlexSans">
+    <div className="h-[92vh] w-full  flex justify-center items-center font-IBMPlexSans ">
       <div className="h-[92.0%] w-[90%]   justify-between flex overflow-hidden items-center flex-col gap-[5.1%]">
         <div className="flex flex-col w-full h-[15.3%]  justify-between">
           <h3 className="text-sm text-primaryDark">
@@ -161,7 +180,7 @@ function BookingForm() {
               style={{
                 width: `${step * 25}%`,
               }}
-              className="h-full w-[30%] bg-secondryBase shadow-md "
+              className="h-full w-[30%] greenGradient1 shadow-md "
             />
           </div>
         </div>
