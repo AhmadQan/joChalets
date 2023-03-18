abb64ef62f821712188d4b0f8ba189dae0fba949
Ahmad Qanadilo <AhmadQan@estartasolutions.com>
2023-03-18 12:32:52 +0300

    adding jochalets domain to headers

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,6 +1,9 @@
 import { createUser, getAll } from "../../../server/controllers/userController";
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "https://www.jochalets.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   const { method } = req;
 
   switch (method) {
