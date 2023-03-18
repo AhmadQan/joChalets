d3a286e4aab305ea50bb30e9cecb5d7f3d6a8988
Ahmad Qanadilo <AhmadQan@estartasolutions.com>
2023-03-18 12:47:31 +0300

    updating the url of the API

--- axios/axiosInstance.js
+++ axios/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const https = axios.create({
   // baseURL: "http://localhost:3000/api/",
-  baseURL: "https://jo-chalets.vercel.app/api/",
+  baseURL: "https://www.jochalets.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
