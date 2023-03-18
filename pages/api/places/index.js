2a9ab665c9c6cb1cd544c724c44611bbe72747c6
Ahmad Qanadilo <AhmadQan@estartasolutions.com>
2023-03-18 12:42:18 +0300

    seting headers

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -4,9 +4,6 @@ import {
 } from "../../../server/controllers/placesController";
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.jochalets.com");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   const { method } = req;
 
   switch (method) {
