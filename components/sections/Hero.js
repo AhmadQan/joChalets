01ce2ec681c5b76ec6845b495e76de28219bdf1a
Ahmad Qanadilo <AhmadQan@estartasolutions.com>
2023-04-15 02:24:53 +0300

    enhancing the hero

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useMedia } from "react-use";
 
@@ -15,29 +15,55 @@ import CupBoldIcon from "../../client/assets/icons/CupBoldIcon";
 import FilterBoldIcon from "../../client/assets/icons/FilterBoldIcon";
 
 import { navVariants } from "../../client/utils/motion";
+import SunFogBoldIcon from "../../client/assets/icons/SunFogBoldIcon";
 
 const initial = {
-  child1: {
-    scale: 1,
-  },
-  child2: {
-    scale: 0.3,
-  },
-  child3: {
-    scale: 0.1,
-  },
+  child1: { x: -160, opacity: 0 },
+  child2: {},
+  child3: {},
 };
 
 function Hero() {
+  const [activeDiv, setActiveDiv] = useState(0);
+
+  const divs = [
+    {
+      key: 1,
+      src: "https://firebasestorage.googleapis.com/v0/b/qanadilodesign.appspot.com/o/other%2FFamilyPoolTime.jpg?alt=media&token=610ee37f-d6d8-4398-8303-512e76582ba7",
+      content: "For The Family",
+      paragraph: "let the Summer fun begin",
+    },
+    {
+      key: 2,
+      src: "https://firebasestorage.googleapis.com/v0/b/qanadilodesign.appspot.com/o/other%2FUntitled%20design.png?alt=media&token=9acef2f1-0905-4d61-8d05-2cc266eb5ea1",
+      content: "Firing up the grill ",
+      paragraph: "for a sizzling summer party ",
+    },
+    {
+      key: 3,
+      src: "https://firebasestorage.googleapis.com/v0/b/qanadilodesign.appspot.com/o/other%2FManWorkingByThePool.png?alt=media&token=c750e462-3985-4cd1-b2e0-2539b66ada1d",
+      content: "Work with a view",
+      paragraph: "soak up the sun by your favorite office",
+    },
+  ];
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setActiveDiv((prevActiveDiv) => (prevActiveDiv + 1) % divs.length);
+    }, 4000); // Change this value to adjust the interval between div switches
+
+    return () => clearInterval(intervalId);
+  }, []);
   const dispatch = useDispatch();
   const isMobile = useMedia("(max-width: 768px)");
 
   return (
     <div
       style={{
-        background: "linear-gradient(to left, #0077be, #3fc3ee)",
+        background:
+          "linear-gradient(169.63deg, #0077BE 1.07%, rgba(63, 195, 238, 0.4) 100.08%)",
       }}
-      className="h-screen w-full shadow-hole flex justify-center  items-end  overflow-hidden rounded-b-20 "
+      className="h-screen bg-white w-full shadow-hole flex justify-center  items-end  overflow-hidden rounded-b-20 "
     >
       <div className="w-full lg:w-[92%] h-[82%] flex flex-col items-end lg:flex-row  relative justify-end">
         <div className="lg:w-[42%] hidden lg:h-full lg:flex lg:flex-col gap-[8vh]">
@@ -83,23 +109,18 @@ function Hero() {
             </div>
           </div>
         </div>
-        <div className=" h-[40%]  px-6 lg:hidden ">
-          <div className="w-full flex flex-col gap-[5vh]">
-            <div className="w-full flex flex-col gap-1">
-              <p className="text-base font-normal  text-primary10">
-                Escape Your Daily Routine
-              </p>
-              <p className="text-2xl font-bold font-IBMPlexSans">
-                Rent a Luxurious Pool House in Jordan Today!
-              </p>
-            </div>
+        <div className=" h-[40%]  w-full px-6 lg:hidden ">
+          <div className="w-full h-full flex flex-col justify-center pt-8 gap-[5vh]">
             <div
               onClick={() => {
                 dispatch(toggleFilterModel());
               }}
-              className="w-full z-20 py-3 aspect-longBton bg-secondry40 mx-auto rounded-2xl border cursor-pointer border-secondry10 shadow-flat flex justify-start gap-3 items-center px-6"
+              className="w-full z-20 py-3 aspect-textField bg-secondry40 mx-auto rounded-2xl border cursor-pointer border-secondry10 shadow-flat flex justify-start gap-3 items-center px-6"
             >
-              <HouseBoldIcon fill={"#ffff"} className={"h-10 aspect-square "} />
+              <FilterBoldIcon
+                fill={"#ffff"}
+                className={"h-10 aspect-square "}
+              />
               <div className="flex flex-col gap-0 items-start">
                 <p className="text-lg font-bold text-primary90">Book Now</p>
                 <p className="text-sm font-medium text-primary90">
@@ -107,100 +128,64 @@ function Hero() {
                 </p>
               </div>
             </div>
+            <SunFogBoldIcon
+              fill={"#F9FFB7"}
+              className={"absolute top-0 right-0 w-[58%] aspect-square"}
+            />
           </div>
         </div>
-        <div className="lg:w-[68%] w-full h-[62%] lg:h-full relative">
+        <div className="lg:w-[68%] w-full h-[68%] lg:h-full relative">
           <AnimatePresence>
-            <motion.div
-              initial={initial.child1}
-              animate={{
-                top: isMobile ? [30, 20, 0, 30] : [300, 220, 0, 300],
-                left: isMobile ? [100, 50, 18, 60] : [500, 250, 0, 500],
-                scale: [0.1, 0.3, 1, 0.1],
-                opacity: [0, 0.4, 1, 0],
-              }}
-              transition={{
-                duration: 15.5,
-                repeat: Infinity,
-                repeatType: "loop",
-                ease: "easeInOut",
-              }}
-              className="w-[84%] lg:w-[58%]  absolute top-0   aspect-square rounded-20 border-2 shadow-elvatedCard border-primary20 bg-white overflow-hidden"
-            >
-              <div className="w-full h-full relative">
-                <img
-                  alt="kid enjoying a swim in the pool"
-                  className="w-full aspect-square object-cover"
-                  src="https://firebasestorage.googleapis.com/v0/b/qanadilodesign.appspot.com/o/other%2FFamilyPoolTime.jpg?alt=media&token=610ee37f-d6d8-4398-8303-512e76582ba7"
-                />
-                <div className="w-full h-[80%] imgOverlayColor absolute  bottom-0 flex justify-start p-10 items-end">
-                  <p className="text-xl font-semibold text-white">
-                    Fun Family <br />
-                    <span className="text-primary90 text-lg">memorys</span>
-                  </p>
-                </div>
-              </div>
-            </motion.div>
-            <motion.div
-              initial={initial.child2}
-              animate={{
-                top: isMobile ? [20, 0, 30, 20] : [220, 0, 300, 250],
-                left: isMobile ? [20, 18, 30, 20] : [250, 0, 500, 250],
-                scale: [0.3, 1, 0.1, 0.3],
-                opacity: [0.4, 1, 0, 0.4],
-              }}
-              transition={{
-                duration: 15.5,
-                repeat: Infinity,
-                repeatType: "loop",
-                ease: "easeInOut",
-              }}
-              className="w-[84%] lg:w-[58%]  absolute top-0   aspect-square rounded-20 border-2 shadow-elvatedCard border-primary20 bg-white overflow-hidden"
-            >
-              <div className="w-full h-full relative">
-                <img
-                  alt="kid enjoying a swim in the pool"
-                  className="w-full aspect-square object-cover"
-                  src="https://firebasestorage.googleapis.com/v0/b/qanadilodesign.appspot.com/o/other%2Frafael-hoyos-weht-Aqot8S_Keb8-unsplash.jpg?alt=media&token=0134da9a-b1ce-4860-b2f4-ce2c23e3449c"
-                />
-                <div className="w-full h-[80%] imgOverlayColor absolute  bottom-0 flex justify-start p-10 items-end">
-                  <p className="text-xl font-semibold text-white">
-                    Fun Family <br />
-                    <span className="text-primary90 text-lg">memorys</span>
-                  </p>
-                </div>
-              </div>
-            </motion.div>
-            <motion.div
-              initial={initial.child3}
-              animate={{
-                top: isMobile ? [0, 30, 20, 0] : [0, 300, 220, 0],
-                left: isMobile ? [18, 30, 20, 18] : [0, 500, 250, 0],
-                scale: [1, 0.1, 0.3, 1],
-                opacity: [1, 0, 0.4, 1],
-              }}
-              transition={{
-                duration: 15.5,
-                repeat: Infinity,
-                repeatType: "loop",
-                ease: "easeInOut",
-              }}
-              className="w-[84%] lg:w-[58%]  absolute top-0   aspect-square rounded-20 border-2 shadow-elvatedCard border-primary20 bg-white overflow-hidden"
-            >
-              <div className="w-full h-full relative">
-                <img
-                  alt="kid enjoying a swim in the pool"
-                  className="w-full aspect-square object-cover"
-                  src="https://images.pexels.com/photos/9044049/pexels-photo-9044049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                />
-                <div className="w-full h-[80%] imgOverlayColor absolute  bottom-0 flex justify-start p-10 items-end">
-                  <p className="text-xl font-semibold text-white">
-                    Fun Family <br />
-                    <span className="text-primary90 text-lg">memorys</span>
-                  </p>
-                </div>
-              </div>
-            </motion.div>
+            {divs.map((div, index) => (
+              <>
+                {activeDiv === index && (
+                  <div className="w-full h-full justify-end">
+                    <motion.div
+                      className="z-20 absolute top-0 px-3 flex flex-col"
+                      initial={initial.child1}
+                      animate={{
+                        x: isMobile ? [-160, 0, 0, 160] : [-500, -250, 0, 500],
+                        opacity: [0, 1, 1, 0],
+                      }}
+                      transition={{
+                        duration: 3.8,
+                        repeatType: "mirror",
+                        ease: "easeIn",
+                      }}
+                    >
+                      <p className="text-3xl text-white font-bold ">
+                        {div?.content}
+                      </p>
+                      <p className="text-xl text-white font-medium mx-5 ">
+                        {div?.paragraph}
+                      </p>
+                    </motion.div>
+                    <motion.div
+                      key={div.key}
+                      initial={initial.child1}
+                      animate={{
+                        x: isMobile ? [160, 20, 20, -160] : [500, 250, 0, -500],
+                        opacity: [0, 1, 1, 0],
+                      }}
+                      transition={{
+                        duration: 3.8,
+                        repeatType: "mirror",
+                        ease: "easeIn",
+                      }}
+                      className="w-[84%] lg:w-[58%]  absolute bottom-0 aspect-square rounded border-2 shadow-elvatedCard border-primary20 bg-white overflow-hidden"
+                    >
+                      <div className="w-full h-full relative">
+                        <img
+                          alt="kid enjoying a swim in the pool"
+                          className="w-full aspect-square object-cover"
+                          src={`${div?.src}`}
+                        />
+                      </div>
+                    </motion.div>
+                  </div>
+                )}
+              </>
+            ))}
           </AnimatePresence>
         </div>
       </div>
