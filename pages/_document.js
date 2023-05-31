b465ace3a5a1251228f6ade3535bcac8143ff19b
Ahmad Qanadilo <AhmadQan@estartasolutions.com>
2023-05-31 20:34:54 +0300

    adding google analytics pit

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        {/* Add your Google Analytics tracking code here */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-C74KCDJSHL"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-C74KCDJSHL');
+          `,
+          }}
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
