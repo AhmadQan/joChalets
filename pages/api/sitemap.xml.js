3e484c2a2dbe822eab20683c9dba8af87b26daee
Ahmad Qanadilo <AhmadQan@estartasolutions.com>
2023-05-31 23:06:25 +0300

    adding a site map v3

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -1,5 +1,7 @@
 import { SitemapStream, streamToPromise } from "sitemap";
 import { Readable } from "stream";
+import { getAll } from "../../server/controllers/placesController";
+import https from "../../axios/axiosInstance";
 
 const generateSitemap = async (req, res) => {
   try {
@@ -11,6 +13,13 @@ const generateSitemap = async (req, res) => {
     smStream.write({ url: "/" });
     smStream.write({ url: "/places" });
     // Add more URLs as needed
+
+    const places = await https.get(`places/`);
+
+    places.forEach((place) => {
+      const url = `/places/${place.id}`; // Adjust the URL structure based on your dynamic route
+      smStream.write({ url });
+    });
 
     smStream.end();
 
